48cb7677b2f3713b68e9f2335610bb39bc504db0
Sr-meraj <marajmia3232@gmail.com>
2024-07-02 14:39:44 +0600

    cors implement

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: ['https://isi-sigma.vercel.app'],
+    methods: ["POST","GET"],
+    credentials: true
+}));
 app.use(bodyParser.json());
 app.use("/assets/files", express.static("assets/files"))
 
